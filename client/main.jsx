7b71a43fe8855688213ce10c61f7ccc356052a5b
Tiffany Wang <thiffanyjun@hotmail.com>
2023-11-29 12:07:20 +1300

    update auth0 config

--- client/main.jsx
+++ client/main.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./styles/input.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles/input.css'
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom";
-import Donut from "./pages/Donut.jsx";
-import DonutList from "./pages/DonutList.jsx";
-import { Auth0Provider } from "@auth0/auth0-react";
+} from 'react-router-dom'
+import Donut from './pages/Donut.jsx'
+import DonutList from './pages/DonutList.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 export const routes = createRoutesFromElements(
-  <Route path={"/"} element={<App />}>
+  <Route path={'/'} element={<App />}>
     <Route index element={<Donut />} />
-    <Route path={"/me"} element={<DonutList />} />
-  </Route>,
-);
+    <Route path={'/me'} element={<DonutList />} />
+  </Route>
+)
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes)
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
-    domain="mako-tiffsdonuts.au.auth0.com"
-    clientId="1gMtmY7xCt6ZtERLwpjCwtAw5NLewfYR"
+    domain="https://mako-2023-tiffany.au.auth0.com"
+    clientId="GLOmMtLCELDxJb4Mu5RRKJMkmRBMs2fo"
     audience="https://tiffsdonuts/api"
     redirectUri={window.location.origin}
   >
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>
-  </Auth0Provider>,
-);
+  </Auth0Provider>
+)
--- server/auth0.ts
+++ server/auth0.ts
@@ -1,12 +1,12 @@
-import { expressjwt as jwt, GetVerificationKey } from "express-jwt";
-import { Request } from "express";
-import { ParamsDictionary } from "express-serve-static-core";
-import { JwtPayload } from "jsonwebtoken";
-import jwks from "jwks-rsa";
+import { expressjwt as jwt, GetVerificationKey } from 'express-jwt'
+import { Request } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { JwtPayload } from 'jsonwebtoken'
+import jwks from 'jwks-rsa'
 
 // TODO: set the domain and audience (API Identifier)
-const domain = "https://mako-tiffsdonuts.au.auth0.com";
-const audience = "https://tiffsdonuts/api";
+const domain = 'https://mako-2023-tiffany.au.auth0.com'
+const audience = 'https://tiffsdonuts/api'
 
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
@@ -17,12 +17,12 @@ const checkJwt = jwt({
   }) as GetVerificationKey,
   audience: audience,
   issuer: `${domain}/`,
-  algorithms: ["RS256"],
-});
+  algorithms: ['RS256'],
+})
 
-export default checkJwt;
+export default checkJwt
 
 export interface JwtRequest<TReq = any, TRes = any>
   extends Request<ParamsDictionary, TRes, TReq> {
-  auth?: JwtPayload;
+  auth?: JwtPayload
 }
